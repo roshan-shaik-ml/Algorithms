519caaa97f205196bd31bf821f4a8efa96bd41c0
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2021-08-18 10:08:23 +0530

    Update ConvexHullBruteForce.py

--- ConvexHullBruteForce.py
+++ ConvexHullBruteForce.py
@@ -1,8 +1,7 @@
 
 # Author: Shaik Faizan Roshan Ali
-# Date: 12th August 2021
-# About : Finding the convex hull of points in an catesian 2-Dimesnional Plane
-# Current State: Bug in counter-clock wise selection in findConvexHull
+# Date: 18th August 2021
+# About : Finding the convex hull of points in a cartesian 2-Dimensional Plane
 
 import random 
 import sys
@@ -22,28 +21,29 @@ def crossProduct(point1, point2, point3):
 def getSlope(point1, point2):
 
     # (y2 - y1) / (x2 - x1)
-    if(point1[0] == point2[0]): # when both x coordinates are same tangent value is infinity
+    if(point1[0] == point2[0]): # when both x coordinates have same tangent value is infinity
 
         return sys.maxsize
     else:
         
         slope = (point2[1] - point1[1]) / (point2[0] - point1[0])
+        # print(atan(slope)) # for printing slope for analysis
         return atan(slope)
 
 def findConvexHull(points):
 
-    stack = []
+    stack = [] # To store the hull
     points.sort() # find the point with least (x, y) coordinates
     print(points)
     stack.append(points.pop(0)) # pop and push the least element into the stack
 
     # Now, For traversing counter-clock wise
-    # We need to sort it by the following priority order
-    # slope, least y value, 
+    # We need to sort points by the following priority order
+    # slope, least y value, x values
     points.sort(key=lambda point: (getSlope(stack[0], point), -point[1], point[0]))
-    # print(stack[0], points) # for checking the first selection point
+    # print(stack[0], points) # test analysis only
 
-    # append the least slope point into the stack.
+    # append the least slope point into the stack connecting the first 2 points.
     stack.append(points.pop(0))
    
     for index in range(0, len(points)):
@@ -56,14 +56,19 @@ def findConvexHull(points):
         else:
 
             stack.append(points[index])
+
+        # print("Stack at interation ", index, ": ", stack) # test analysis only
     
     return stack
 
+# the plotGraph function is used for visualisation of the given coordinates.
 def plotGraph(points):
 
     x = [point[0] for point in points]
     y = [point[1] for point in points]
     plt.scatter(x, y)
+
+    # labeling coordinate values using plt.text method.
     for index in range(0, len(x)):
 
         plt.text(x[index], y[index], str((x[index], y[index])))
@@ -71,11 +76,8 @@ def plotGraph(points):
 
 if __name__ == '__main__':
 
-    # points = [(random.randint(0, 10), random.randint(0, 10)) for i in range(10)] # taking random points
-    points = [(0, 8), (1, 3), (3, 9), (5, 4), (6, 10), (8, 1), (8, 9), (9, 0), (10, 1), (10, 3)]
-    
-    coordinates = points.copy()
-    
+    points = [(3, 4), (3, 10), (5, 8), (5, 9), (6, 3), (7, 4), (7, 10), (8, 1), (8, 1), (9, 7)]
+    coordinates = points.copy() # for plotting
     hull = findConvexHull(points)
-    print("hull: ", hull)
-    plotGraph(coordinates) # to visualise the coordinates
+    print("hull coordinates: ", hull)
+    plotGraph(coordinates)
